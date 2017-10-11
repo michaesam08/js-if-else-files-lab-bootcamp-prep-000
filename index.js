0e6ab984430acae344d7e7605dc1c2cf13b1aac0
Michael Samara <michaelsam08@gmail.com>
2017-10-11 16:12:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (name === "Susan") {
 } else {
     console.log("Expected the name to be Susan")
 }
-
+if (height === "74") {console.log("The height is correct")}
 // Don't worry about this
 module.exports = { name, height
 }
